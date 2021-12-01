37a001eb3fc1cc07feeca1d10dfa879c446e4788
Msola <msolabsch@gmail.com>
2021-12-01 18:05:22 +0100

    sasd

---
+++ src/main/java/com/example/apiAnime/repository/AnimeRepository.java
@@ -0,0 +1,10 @@
+package com.example.apiAnime.repository;
+
+import com.example.apiAnime.domain.model.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AnimeRepository extends JpaRepository<Anime, UUID> {
+
+}
